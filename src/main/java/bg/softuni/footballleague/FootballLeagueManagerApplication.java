bdcde18668f59ab215792eacf8919162dcd8e41c
Valeri-Parvanov <valyo.parvanov@gmail.com>
2026-06-26 19:25:28 +0300

    Initial Spring Boot project skeleton

---
+++ src/main/java/bg/softuni/footballleague/FootballLeagueManagerApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.footballleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballLeagueManagerApplication.class, args);
+	}
+
+}
