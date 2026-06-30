51e7cc5dcee05de16bbd9039ad2b1ddb40a31872
Valeri-Parvanov <valyo.parvanov@gmail.com>
2026-06-30 08:22:13 +0300

    refactor: replace wildcard jakarta.persistence imports with explicit ones

--- src/main/java/bg/softuni/footballleague/model/Match.java
+++ src/main/java/bg/softuni/footballleague/model/Match.java
@@ -1,6 +1,13 @@
 package bg.softuni.footballleague.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
